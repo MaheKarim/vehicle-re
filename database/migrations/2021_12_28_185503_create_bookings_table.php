3b397765243b24ed3b2ea552208b918803467d30
Mahe Karim <mahekarim@deligram.com>
2021-12-29 01:02:32 +0600

    Booking Model - Controller Added

---
+++ database/migrations/2021_12_28_185503_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('service_center_id');
+            $table->date('booking_date')->nullable();
+            $table->tinyInteger('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
