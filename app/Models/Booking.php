f55b1ccbb6a053553087e3d1c76d543ff3cd1b1c
Mahe Karim <mahekarim@deligram.com>
2021-12-29 01:06:09 +0600

    Booking Relations Model Added

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,5 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
     protected $guarded = [' '];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function service_center()
+    {
+        return $this->belongsTo(ServiceCenter::class);
+    }
 }
